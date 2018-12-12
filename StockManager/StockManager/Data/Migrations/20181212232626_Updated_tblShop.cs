94f01ef0623908f936c6bac618b1ab105bb38dbc
Darren Hannay <42913620+DarrenHannay2019@users.noreply.github.com>
2018-12-12 23:29:32 +0000

    Updated Shop Model for table tblShops

---
+++ StockManager/StockManager/Data/Migrations/20181212232626_Updated_tblShop.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace StockManager.Data.Migrations
+{
+    public partial class Updated_tblShop : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "WarehouseTown",
+                table: "Shop",
+                newName: "ShopTown");
+
+            migrationBuilder.RenameColumn(
+                name: "WarehouseTelephone",
+                table: "Shop",
+                newName: "ShopTelephone");
+
+            migrationBuilder.RenameColumn(
+                name: "WarehouseStreet",
+                table: "Shop",
+                newName: "ShopStreet");
+
+            migrationBuilder.RenameColumn(
+                name: "WarehousePostCode",
+                table: "Shop",
+                newName: "ShopPostCode");
+
+            migrationBuilder.RenameColumn(
+                name: "WarehouseName",
+                table: "Shop",
+                newName: "ShopName");
+
+            migrationBuilder.RenameColumn(
+                name: "WarehouseMemo",
+                table: "Shop",
+                newName: "ShopMemo");
+
+            migrationBuilder.RenameColumn(
+                name: "WarehouseFax",
+                table: "Shop",
+                newName: "ShopFax");
+
+            migrationBuilder.RenameColumn(
+                name: "WarehouseArea",
+                table: "Shop",
+                newName: "ShopArea");
+
+            migrationBuilder.RenameColumn(
+                name: "DefaultWarehouse",
+                table: "Shop",
+                newName: "Clearance");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ShopTown",
+                table: "Shop",
+                newName: "WarehouseTown");
+
+            migrationBuilder.RenameColumn(
+                name: "ShopTelephone",
+                table: "Shop",
+                newName: "WarehouseTelephone");
+
+            migrationBuilder.RenameColumn(
+                name: "ShopStreet",
+                table: "Shop",
+                newName: "WarehouseStreet");
+
+            migrationBuilder.RenameColumn(
+                name: "ShopPostCode",
+                table: "Shop",
+                newName: "WarehousePostCode");
+
+            migrationBuilder.RenameColumn(
+                name: "ShopName",
+                table: "Shop",
+                newName: "WarehouseName");
+
+            migrationBuilder.RenameColumn(
+                name: "ShopMemo",
+                table: "Shop",
+                newName: "WarehouseMemo");
+
+            migrationBuilder.RenameColumn(
+                name: "ShopFax",
+                table: "Shop",
+                newName: "WarehouseFax");
+
+            migrationBuilder.RenameColumn(
+                name: "ShopArea",
+                table: "Shop",
+                newName: "WarehouseArea");
+
+            migrationBuilder.RenameColumn(
+                name: "Clearance",
+                table: "Shop",
+                newName: "DefaultWarehouse");
+        }
+    }
+}
--- StockManager/StockManager/Models/Shop.cs
+++ StockManager/StockManager/Models/Shop.cs
@@ -20,34 +20,43 @@ namespace StockManager.Models
         [Display(Name = "Shop Code")]
         [StringLength(10, MinimumLength =2)]
         public string ShopRef { get; set; }
-        [Display(Name = "Name")]
+        [Display(Name = "Shop Name")]
+        [Column("ShopName")]
         [StringLength(10)]
         [Required]
         public string WarehouseName { get; set; }
         [Display(Name = "Street")]
+        [Column("ShopStreet")]
         [StringLength(60)]
         public string WarehouseStreet { get; set; }
         [Display(Name = "Area")]
+        [Column("ShopArea")]
         [StringLength(60)]
         public string WarehouseArea { get; set; }
+        [Column("ShopTown")]
         [Display(Name = "Town")]
         [StringLength(60)]
         public string WarehouseTown { get; set; }
+        [Column("ShopPostCode")]
         [Display(Name = "Post Code")]
         [StringLength(11)]
         public string WarehousePostCode { get; set; }
+        [Column("ShopTelephone")]
         [Display(Name = "Telephone")]
         [StringLength(16)]
         public string WarehouseTelephone { get; set; }
+        [Column("ShopFax")]
         [Display(Name = "Fax")]
         [StringLength(16)]    
         public string WarehouseFax { get; set; }
+        [Column("ShopMemo")]
         [Display(Name = "Memo")]
         public string WarehouseMemo { get; set; }
         [Display(Name = "Shop Type")]
         [Required]
         public SHType? SHopType { get; set; }
         [Display(Name = "Clearance")]
+        [Column("Clearance")]
         public bool DefaultWarehouse { get; set; }
         [Display(Name = "Created By")]
         public int CreatedBy { get; set; }
