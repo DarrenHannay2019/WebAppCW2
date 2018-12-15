fa5901a464e36e0f76c48f29e1d12ede1cbf0cfe
Darren Hannay <21702155@bucks.ac.uk>
2018-12-15 11:16:23 +0000

    Added the Start of Modifying the Login System for the web application

--- StockManager/StockManager/Program.cs
+++ StockManager/StockManager/Program.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using StockManager.Models;  // StockManagerContext
+using Microsoft.Extensions.DependencyInjection; // create the scope
+using Microsoft.Extensions.Logging;
 
 namespace StockManager
 {
@@ -14,7 +17,23 @@ namespace StockManager
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            // CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<AppIdentityDbContext>();
+                    context.Database.EnsureCreated();
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred creating the DB.");
+                }
+            }
+            host.Run();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
--- StockManager/StockManager/Startup.cs
+++ StockManager/StockManager/Startup.cs
@@ -20,6 +20,7 @@ namespace StockManager
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+
         }
 
         public IConfiguration Configuration { get; }
